b3ce4fa59087f3b48519bd58834247d5cb6b3f29
Hossam <hossam.mohamed@shelter-technology.com>
2025-01-21 23:44:40 +0200

    update requirements file

--- backend/api.py
+++ backend/api.py
@@ -265,7 +265,7 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     hashed_password = get_password_hash(user.password)
-    new_user = User(email=user.email, hashed_password=hashed_password)
+    new_user = User(email=user.email, password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
